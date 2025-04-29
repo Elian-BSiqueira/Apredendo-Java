3899a54656f806aec5375a98fd4971e51656e322
Elian-BSiqueira <elian.b.siqueira@gmail.com>
2025-04-29 11:25:15 -0300

    exericicios de repeticao

--- src/Exercicios/EstruturaDeRepeticao/Ex07.java
+++ src/Exercicios/EstruturaDeRepeticao/Ex07.java
@@ -2,13 +2,14 @@ package Exercicios.EstruturaDeRepeticao;
 
 import java.util.Scanner;
 
-public class Ex05 {
+public class Ex07 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        System.out.print("Digite um numero para ver sua tabuada: ");
+        System.out.print("Digite um numero para saber seu fatorial: ");
         int numero = scan.nextInt();
-        for (int i = 1; i < 11; i++) {
-            System.out.printf("%d x %d = %d %n", numero, i, numero * i);
+        for (int i = 1; i <= numero ; i++) {
+            System.out.printf("");
+
         }
     }
 }
