6da34f29b4747d31e1784c6472ca65ad55191b65
Elian-BSiqueira <elian.b.siqueira@gmail.com>
2025-04-30 18:34:14 -0300

    update exercicios
    Movido exercicios do arquivo Ex07 para o arquivo principal

--- src/Exercicios/EstruturaDeRepeticao/Ex01a11.java
+++ src/Exercicios/EstruturaDeRepeticao/Ex01a11.java
@@ -1,8 +1,6 @@
 package Exercicios.EstruturaDeRepeticao;
 
-import Exercicios.util.Funcoes;
-
-public class Ex01a06 {
+public class Ex01a11 {
     public static void main(String[] args) {
         // Exercicio 1
 
@@ -53,7 +51,7 @@ public class Ex01a06 {
         System.out.printf("%d! = %d", numero, fatorial);*/
 
         //Exercicio 8 - maior numero
-        int[] numeros = {3, 7, 3, 43, 6, 100, 2, 5};
+        /*int[] numeros = {3, 7, 3, 43, 6, 100, 2, 5};
         int maiorNumero = numeros[0];
         for (int num : numeros) {
             if (num > maiorNumero) {
@@ -62,9 +60,37 @@ public class Ex01a06 {
                 {};
             }
         }
-        System.out.println("Maior numero " + maiorNumero);
+        System.out.println("Maior numero " + maiorNumero);*/
 
         // Exercicio 9 -
+        /*int[] numeros = {10, 20, 30, 40, 50};
+        for (int num : numeros) {
+            System.out.print(" " + num);
+        }*/
+
+        //Exercicio 10
+        /*String[] palavras = {"casa", "carro", "avião", "ônibus", "barco"};
+        int contador = 0;
+        for (String palavra : palavras) {
+            if (palavra.length() > 4) {
+                contador ++;
+            }
+        }
+
+        System.out.println("Palavras com mais de 4 letras é " + contador);*/
+
+        //Exercicio 11
+        /*int verificarNumero = Funcoes.VerificarNumero("Digite um numero para verificar se ele esta na lista: ");
+        int[] numeros = {3, 7, 11, 12, 25};
+        int contador = 0;
+        for (int num : numeros) {
+            if (num == verificarNumero) {
+                contador ++;
+            }
+        }
+        System.out.printf("O numero %d foi encontrado %d vez ", verificarNumero, contador);*/
+
+
 
 
 
