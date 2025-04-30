3801dec543b70d0b1c9eb2455d22dfffe564b3e3
Elian-BSiqueira <elian.b.siqueira@gmail.com>
2025-04-30 09:14:54 -0300

    exclusao de arquivos

--- src/Exercicios/EstruturaDeRepeticao/Ex04.java
+++
@@ -1,9 +0,0 @@
-package Exercicios.EstruturaDeRepeticao;
-
-public class Ex04 {
-    public static void main(String[] args) {
-        for (int i = 5; i >= 0 ; i--) {
-            System.out.println(i);
-        }
-    }
-}
