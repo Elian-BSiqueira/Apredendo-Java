b523772f1a31b06064f87522d9db39ead1dae176
Elian-BSiqueira <elian.b.siqueira@gmail.com>
2025-05-05 21:44:28 -0300

    Update testes.java

--- src/testes.java
+++ src/testes.java
@@ -1,3 +1,5 @@
+import Exercicios.util.Funcoes;
+
 import javax.swing.*;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -5,18 +7,8 @@ import java.util.Scanner;
 
 public class testes {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Escreva ");
-        String teste = scan.nextLine().toLowerCase();
-        String[] vogais = {"a", "e", "i", "o", "u"};
-        char[] v = {'a'};
-        int quantidade = 0;
-
-
-        System.out.println(quantidade);
-
-
-
+        int numero = Funcoes.VerificarInteiroComIntervalo("Digite", 1, 5);
+        System.out.println(numero);
     }
 
 }
