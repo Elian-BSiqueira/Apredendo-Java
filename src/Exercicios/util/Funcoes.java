695693a42bfe207ecefc4fd0f69edb08c2617e76
Elian-BSiqueira <elian.b.siqueira@gmail.com>
2025-05-05 21:42:33 -0300

    Update Funcoes.java
    Adicionado uma funcao para verificar o inteiro e se ele esta ou nao dentro de um intervalo

--- src/Exercicios/util/Funcoes.java
+++ src/Exercicios/util/Funcoes.java
@@ -24,6 +24,30 @@ public class Funcoes {
         return numero;
     }
 
+    public static int VerificarInteiroComIntervalo(String texto, int minimo, int maximo) {
+        Scanner scan = new Scanner(System.in);
+        boolean controleDeLoop = true;
+        int numero = 0;
+        do {
+            System.out.print(texto);
+            try {
+                numero = scan.nextInt();
+                if (numero >= minimo && numero <= maximo) {
+                    controleDeLoop = false;
+                }
+                else {
+                    System.out.println("Opcao Invalida. Digite um numero entre 1 e 5");
+                }
+
+            } catch (InputMismatchException e) {
+                scan.nextLine();
+                System.out.println("Entrada invalida. Digite um numero");
+            }
+        } while (controleDeLoop);
+
+        return numero;
+    }
+
     public static double VerificarNumeroDouble(String texto) {
         Scanner scan = new Scanner(System.in);
         boolean controleDeLoop = true;
@@ -44,3 +68,4 @@ public class Funcoes {
         return numero;
     }
 }
+
